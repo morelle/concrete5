fd3dc35b7c9c9c0d3e4a2dc1f0a7357dca02c082
morelle <morelle-romain@hotmail.fr>
2013-01-23 17:10:24 +0100

    un petit commit !
    Signed-off-by: morelle <morelle-romain@hotmail.fr>

---
+++ config/site.php
@@ -0,0 +1,6 @@
+<?php 
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'bdd');
+define('PASSWORD_SALT', 'oiE13hsKpiZFFJZnOpL4tpHaZVSiFP60U7Fcn0DKkh5ECUoSZAsLsK4wH2HW1Co4');
